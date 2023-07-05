4fa7cb523224f28aef283a19a0899fedcf7d74e0
Laurent Destailleur <eldy@destailleur.fr>
2023-07-05 22:37:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,8 +61,12 @@ fi
 
 if [ "x$GITHUB_EVENT_NAME" == "xpull_request" ]; then
    git branch
+   git symbolic-ref pull/${GITHUB_REF_NAME} origin/${GITHUB_HEAD_REF}
+   git branch
    git symbolic-ref refs/remotes/origin/HEAD origin/${GITHUB_BASE_REF}
-   export STAGED_FILES_CMD=$(git --no-pager diff --name-only origin/${GITHUB_HEAD_REF} origin/${GITHUB_BASE_REF})
+   git branch
+   #export STAGED_FILES_CMD=$(git --no-pager diff --name-only origin/${GITHUB_HEAD_REF} origin/${GITHUB_BASE_REF})
+   export STAGED_FILES_CMD=$(git --no-pager diff --name-only pull/${GITHUB_REF_NAME} origin/${GITHUB_BASE_REF})
    echo STAGED_FILES_CMD=$STAGED_FILES_CMD
 fi
 
